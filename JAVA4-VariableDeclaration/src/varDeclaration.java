b313044e0a56f75dc1b85b00ba1dad22226bcea6
TestSheepNZ <mike_talks@hotmail.com>
2016-08-23 20:57:36 +1200

    Done

---
+++ JAVA4-VariableDeclaration/src/varDeclaration.java
@@ -0,0 +1,25 @@
+/*
+ * This program is part of my learning Java series
+ * This one looks at variable declaration
+ * 
+ * Find the blog here,
+ * http://testsheepnz.blogspot.co.nz/2016/08/java-3-debugging-code-doesnt-bother-me.html
+ * 
+ * For more information - please reread.
+ * 
+ * Mike Talks, Aug 2016
+ */
+
+public class varDeclaration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int id_number = 42781;
+		double pi_num = 3.142;
+		String print_string = "Hello, World";
+
+		System.out.println(id_number);
+		System.out.println(pi_num);
+		System.out.println(print_string);
+	}
+}
